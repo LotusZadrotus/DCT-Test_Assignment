0c15c054e783437f9fffd1bbadf718268766ff41
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-06-20 17:22:54 +0300

    Create MainPage ViewModel

---
+++ CryptoInfo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Documents;
+using CryptoInfo.Annotations;
+using CryptoInfo.Models;
+using CryptoInfo.Services;
+
+namespace CryptoInfo.ViewModels;
+
+public class MainPageViewModel: BaseViewModel
+{
+    private ObservableCollection<Asset> _assets;
+    public ObservableCollection<Asset> Assets
+    {
+        get => _assets;
+        set
+        {
+            _assets = value;
+            OnPropertyChanged(nameof(Assets));
+        }
+    }
+
+    private AssetsService _service;
+    public MainPageViewModel()
+    {
+        _service = new AssetsService();
+        _assets = _service.GetAssets();
+    }
+
+    public override void Refresh()
+    {
+        Assets = _service.GetAssets();
+    }
+}
