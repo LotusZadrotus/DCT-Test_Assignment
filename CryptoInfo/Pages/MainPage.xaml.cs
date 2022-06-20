57ff04b0b075211c3e219cfe078d572d774f6e98
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-06-20 17:22:02 +0300

    Create MainPage layout

---
+++ CryptoInfo/Pages/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using CryptoInfo.Models;
+using CryptoInfo.ViewModels;
+
+namespace CryptoInfo.Pages;
+
+public partial class MainPage : Page
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void EventSetter_OnHandler(object sender, MouseButtonEventArgs e)
+    {
+        var view = sender as ListView;
+        var asset = view.SelectedItem as Asset;
+        Services.NavigationService.NavigateToAsset(asset.Id);
+    }
+}
