6194c1236286c587672aa473066e3e513890cf87
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-06-20 17:24:27 +0300

    Create SearchViewModel

---
+++ CryptoInfo/ViewModels/SearchViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CryptoInfo.Models;
+using CryptoInfo.Services;
+
+namespace CryptoInfo.ViewModels;
+
+public class SearchViewModel : BaseViewModel
+{
+    private ObservableCollection<Asset> _assets= new();
+    private readonly AssetsService _service;
+    public ObservableCollection<Asset> Assets
+    {
+        get => _assets;
+        set
+        {
+            _assets = value;
+            OnPropertyChanged(nameof(Assets));
+        } 
+    }
+    
+    public ICommand Search { get; set; }
+    public SearchViewModel()
+    {
+        _service = new AssetsService();
+        Search = new RelayCommand(SearchMethod);
+    }
+
+    private void SearchMethod(object name)
+    {
+        Assets = _service.GetAssets(name as string);
+    }
+
+
+    
+    public override void Refresh()
+    {
+        base.Refresh();
+    }
+    
+}
