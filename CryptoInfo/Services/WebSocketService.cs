a43ae62ce4ff4367d22f172c733d2217cfcea27e
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-06-22 15:12:32 +0300

    Added WebSocket Service to track info online

---
+++ CryptoInfo/Services/WebSocketService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using CryptoInfo.Models;
+using CryptoInfo.ViewModels;
+using Newtonsoft.Json.Linq;
+
+namespace CryptoInfo.Services;
+
+public class WebSocketService: IDisposable
+{
+    private const string API_URL = @"wss://ws.coincap.io/prices?assets=";
+    private string _assets = "";
+    private ClientWebSocket _socket;
+    private readonly object _sync = new();
+
+    public delegate void Update(JObject updates);
+
+    public event Update OnUpdate;
+    public WebSocketService(List<Asset> assets)
+    {
+        foreach (var item in assets)
+        { 
+            
+            _assets = $"{_assets}{item.Id},";
+        }
+
+        _assets=_assets.TrimEnd(',');
+        _socket = new ClientWebSocket();
+    }
+    
+
+    public async void Connect()
+    {
+        await _socket.ConnectAsync(new Uri(API_URL + _assets), CancellationToken.None);
+        // MessageBox.Show(_assets);
+        await Echo();
+    }
+
+    private async Task Echo()
+    {
+        var buffer = new byte[1024 * 4];
+        var result = await _socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        while (!result.CloseStatus.HasValue)
+        {
+            await Task.Run(() =>
+            {
+                var json = JObject.Parse(Encoding.UTF8.GetString(buffer));
+                OnUpdate.Invoke(json);
+                buffer = new byte[1024 * 4];
+            });
+            result = await _socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+    }
+
+    public void Close()
+    {
+        _socket.CloseAsync(WebSocketCloseStatus.NormalClosure,"",CancellationToken.None);
+    }
+
+    public void Dispose()
+    {
+        _socket.Dispose();
+        Close();
+    }
+}
--- CryptoInfo/ViewModels/MainPageViewModel.cs
+++ CryptoInfo/ViewModels/MainPageViewModel.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Documents;
 using CryptoInfo.Annotations;
 using CryptoInfo.Models;
 using CryptoInfo.Services;
+using Newtonsoft.Json.Linq;
 
 namespace CryptoInfo.ViewModels;
 
 public class MainPageViewModel: BaseViewModel
 {
     private ObservableCollection<Asset> _assets;
+    private WebSocketService _webSocket;
     public ObservableCollection<Asset> Assets
     {
         get => _assets;
@@ -31,6 +34,9 @@ public class MainPageViewModel: BaseViewModel
         {
             _service = new AssetsService();
             _assets = _service.GetAssets();
+            _webSocket = new WebSocketService(_assets.ToList());
+            _webSocket.Connect();
+            _webSocket.OnUpdate += UpdateHandler;
         }
         catch (Exception e)
         {
@@ -38,6 +44,14 @@ public class MainPageViewModel: BaseViewModel
         }
     }
 
+    private void UpdateHandler(JObject items)
+    {
+        foreach (var item in items)
+        {
+            var data = _assets.FirstOrDefault(x => x.Id == item.Key);
+            if (data is not null) data.PriceUsd = Convert.ToDecimal(item.Value);
+        }
+    }
     public override void Refresh()
     {
         try
